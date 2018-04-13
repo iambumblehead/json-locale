b24f891cd43cf947d833c7ae0c1892e2ca4746f8
chris <chris@bumblehead.com>
2018-04-13 00:49:40 -0700

    begin moving files

--- index.js
+++
@@ -1,9 +0,0 @@
-var JSONLocale = require('./json-locale'),
-    UserOptions = require('./lib/UserOptions.js');
-
-module.exports = {
-  convert : function (ops, fn) {
-    var o = UserOptions.getNew(ops);
-    JSONLocale.convert(o, fn);
-  }  
-};
